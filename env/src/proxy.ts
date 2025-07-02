791b41051f574175eb2c1a6935f04c7bcdf7e88a
Daniel Wei <danielwii0326@gmail.com>
2025-07-02 20:24:07 +0900

    fix: agent type issue

--- env/src/proxy.ts
+++ env/src/proxy.ts
@@ -1,15 +1,17 @@
 import { HttpsProxyAgent } from 'https-proxy-agent';
 import { Logger } from '@nestjs/common';
-import { ProxyAgent } from 'undici';
 
 import { SysEnv } from './configure';
+import { ProxyAgent } from 'undici';
 import https from 'node:https';
 import { f } from '@app/utils';
+
+import type { Agent } from 'node:http';
 
 const proxy = SysEnv.APP_PROXY_ENABLED ? `${SysEnv.APP_PROXY_HOST}:${SysEnv.APP_PROXY_PORT}` : '';
 export const SysProxy = {
   proxy,
-  agent: SysEnv.APP_PROXY_ENABLED ? new HttpsProxyAgent(proxy) : undefined,
+  agent: SysEnv.APP_PROXY_ENABLED ? (new HttpsProxyAgent(proxy) as Agent) : undefined,
   unauthorizedAgent: SysEnv.APP_PROXY_ENABLED
     ? new HttpsProxyAgent(proxy, { rejectUnauthorized: false })
     : new https.Agent({ rejectUnauthorized: false }),
